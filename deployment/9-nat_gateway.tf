108e29fd9169387c17da6f42531cab80db32a4f6
uekyo <kyo@viv.co.jp>
2024-03-13 12:12:25 -0700

    Deploy terraform files

---
+++ deployment/9-nat_gateway.tf
@@ -0,0 +1,11 @@
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-nat-gw" })
+  )
+}
+
+// Need Make another NAT Gateway
